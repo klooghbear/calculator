15247a2381676880977c93544ddd50534d82741b
sebastiankloogh@gmail.com <sebastiankloogh@gmail.com>
2019-12-13 15:46:43 +1300

    Add html & css framework

--- calculator.js
+++ calculator.js
@@ -1,3 +1,15 @@
+//My calculator should contain a clickable element containing an "=" corresponding id
+
+//My calculator should contain 10 clickable elements containing one number each from 0-9, with the following corresponding IDS id = zero, id = one, id = two, id = three, id = four, id = five id = six, id = seven, id = eight, id = nice
+
+//My calculator should contain 4 clickable elements containg one of the 4 primary operators with the following corresponding IDS id = add, id = subtract, id = multiple, id = divide
+
+//My calculator should contain a clickable element contaning a decimal point symbol witha  conrresponding id = decimal
+
+//My calculator should contain a clickable element with an id = clear
+
+//My calculator should contain an element to display values with a corresponding id = display
+
 //At any present time, pressing the clear button cleas the input and output values, and retirn the calculator to its initialized state; 0 should be shown in the element with the id of display
 
 //As i input numbers, i should be able to see my input in the elemnet with the id of display
