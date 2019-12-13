0acc177f74eb6513926bc064f4b6d4874653c669
sebastiankloogh@gmail.com <sebastiankloogh@gmail.com>
2019-12-13 14:20:57 +1300

    add pseudocode

---
+++ calculator.js
@@ -0,0 +1,19 @@
+//At any present time, pressing the clear button cleas the input and output values, and retirn the calculator to its initialized state; 0 should be shown in the element with the id of display
+
+//As i input numbers, i should be able to see my input in the elemnet with the id of display
+
+//In any order, I should be able to add, stubtract, multiply, and divide a chain of numbers of any length, and whien I hit =, the correct result should be shown in the element with the id of display
+
+//When inputting numbers, my calculator should not allow a number to begin with mupltiple zeros
+
+//When the decimal element is clicked, a should append to the currently displayed value; two . in one number should not be accepted.
+
+//I should be able to perform and opeation (+, - , *. /) on numbers containing decimal points
+
+//If 2 or more operatirs are entered consecutively, the operation performed should be the last operator entered (excluding the negative (-)sign). For example if 5 + * 7 = is entered the result should be 35 (ie 5*7); if 5 * - 5 = is entered the result should be -25(ie. 5 * (-5))
+
+//Pressing an operator immediately following = should start a new calculation that operates on the result of the previous evaluation
+
+//My Calculator should have several decimal places of precision when it comes to rounding ( note that there is no exact standard, but your should be able to handle calculations like 2 / 7 with reasonable precision to at least 4 decimal places
+
+
