068ca6729826f8554daaed23a823599e97587e3b
sebastiankloogh@gmail.com <sebastiankloogh@gmail.com>
2019-12-16 10:31:54 +1300

    redo pseudo and delete functions

--- calculator.js
+++ calculator.js
@@ -2,64 +2,27 @@
 //A calculator is a device in which a user enters a series of commands in order to calculates mathematical equations.
 //User enters a series of numbers, a math operator and an "=" button in order to solve an equation.
 
-document.addEventListener('DOMContent', initialiseCalculator)
 
-//create variables to store information into an array, store result of an equation and the current number
+//Create varaiables to help store targeted DOM Elements
+let display = document.addEventListener();
+let decimal = document.addEventListener();
+let clear = document.addEventListener();
+let numbers = document.addEventListener();
+let operator = document.addEventListener();
+
+//create variables to store information
 let entries = [];
 let currentNumber = '';
 let result = 0;
 
-//initialise the display which will include functions
-function initialiseCalculator() {
-    inputNumber();
-}
+//Add event listeners to all buttons
 
-//function to get number input based on user click
-function inputNumber() {
-    let number = document.getElementsByClassName("number");
-    for(i = 0; i < number.length; i++){
-        number[i].addEventListener('click', clickNumber);
-    }
-}
+//Create a function that will update display
 
-//function to return number output based on input
-function clickNumber(event) {
-    let value = event.target.innerHTML;
-    currentNumber = currentNumber + value;
-    entries.push(currentNumber)
-    updateDisplay();
-}
-
-//function to get operator input based on user click 
-function inputOperator() {
-    let operator = document.getElementsByClassName('operator');
-    for(i = 0; i < operator.length; i++){
-        operator[i].addEventListener('click', clickOperator);
-    }
-}
-
-function clickOperator(event) {
-    let mathOperator = event.target.innerHTML;
-    entries.push(operator);
-    currentNumber = "";
-    updateDisplay();
-}
-
-//function to display the string on screen
-function updateDisplay() {
-    let.display = document.getElementById('display');
-    display.innerHTML = currentNumber;
-}
+//Create function that will perform the mathematical operation for us
 
 //function for allClear to wipe entries
-function allClear () {
-    currentNumber = '';
-    entries = [];   
-    display.value = 0;
-}
 
 //clear the current working display
-function clear () {
-    currentNumber = '';
-    display.value = 0;
-}
+
+//Create a function that will insert decimal points
