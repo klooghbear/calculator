d53ca88487e71872bf5ae7ff31fcde7e15becd47
sebastiankloogh@gmail.com <sebastiankloogh@gmail.com>
2019-12-16 17:39:23 +1300

    Add clear functions

--- calculator.js
+++ calculator.js
@@ -2,74 +2,88 @@
 //A calculator is a device in which a user enters a series of commands in order to calculates mathematical equations.
 //User enters a series of numbers, a math operator and an "=" button in order to solve an equation.
 
-
 //Create varaiables to help store targeted DOM Elements
 let display = document.getElementById('display');
 let decimal = document.getElementById('decimal');
-let clear = document.getElementById('clear');
-let numbers = document.getElementsByClassName('number');
-let operators = document.getElementsByClassName('operator');
+let AC = document.getElementById('allClear');
+let CE = document.getElementById('clear');
 
 //create variables to store information
 let entries = [];
-let tempValue = '0';
-let tempVal;
+let numberString = '0';
 let isPreviousResult = false;
 
-
-//Add event listeners to all buttons
-for(let i = 0; i < numbers.length; i++){
-    numbers[i] = document.addEventListener('click', updateDisplay) //,function)
+//Add event listeners to all numbers
+function listenForNumbers() {
+    let numbers = document.getElementsByClassName('number');
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i] = document.addEventListener('click', updateNumber);
+    }
 }
 
-for(let i = 0; i < operators.length; i++){
-    operators[i] = document.addEventListener('click', calculate) //,function)
+//Add event listener for all operators
+function listenForOperators() {
+    let operators = document.getElementsByClassName('opertor');
+    for (let i = 0; i < operators.length; i++) {
+        operators[i] = document.addEventListener('click', calculate) //,function)
+    }
 }
 
 //Create a function that will update display
-function updateDisplay (event) {
+function updateNumber(event) {
     let buttonClick = event.target.innerHTML;
-    if(tempValue === "0"){
-        tempValue = "";
-    }
-    //update content of button clicked
-    tempValue += buttonClick;
-    display.innerHTML = tempValue;
+    numberString += buttonClick;
+    display.innerHTML = numberString;
 }
 
 //Create function that will perform the mathematical operation for us
-function calculate () {
-    
-    entries.push(tempValue)
-    
+function calculate() {
+
+    entries.push(numberString)
+
     let currentNumber = Number(entries[0]);
 
-    for(var i = 0; i < entries.length; i++){
+    for (var i = 0; i < entries.length; i++) {
         let nextNumber = Number(entries[i + 1]);
         let mathOperator = entries[i]
-        if(mathOperator === '+') {
+        if (mathOperator === '+') {
             return currentNumber += nextNumber;
-        } else if (mathOperator === '-'){
+        } else if (mathOperator === '-') {
             return currentNumber -= nextNumber;
-        } else if (mathOperator === '÷'){
+        } else if (mathOperator === '÷') {
             return currentNumber /= nextNumber;
-        } else if (mathOperator === 'x'){
+        } else if (mathOperator === 'x') {
             return currentNumber *= nextNumber;
         }
     }
 
-    if(currentNumber < 0) {
+    if (currentNumber < 0) {
         currentNumber = Math.abs(currentNumber) + '-';
     }
 
     display.value = currentNumber;
-    tempValue = JSON.stringify(currentNumber);
+    numberString = JSON.stringify(currentNumber);
     isPreviousResult = true;
     entries = [];
 }
 
 //function for allClear to wipe entries
+function allClear() {
+    numberString = '';
+    entries = [];
+    display.value = '0';
+}
 
 //clear the current working display
+function clear() {
+    numberString = '';
+    display.value = '0';
+}
 
 //Create a function that will insert decimal points
+function decimalPoint() {
+    if (!numberString.includes('.')) {
+        numberString += '.';
+    }
+    display.innerHTML = numberString;
+}
